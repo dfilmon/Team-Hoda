419a713bec9d72e82792fd196ce03f905ae32863
dfilmon <abraha.filmon@gmail.com>
2022-12-06 19:21:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Team-Hoda
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ekgvjs)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import Nav from './Nav';
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
